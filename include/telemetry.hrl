ecf7956aabdd2ff5b29cea208256c91b265d7890
Artem Teslenko <ates@ipv6.dp.ua>
2019-01-12 14:06:25 +0200

    Fix ?update_counter macro

--- include/telemetry.hrl
+++ include/telemetry.hrl
@@ -1,3 +1,3 @@
--define(update_counter(Name), telemetry:update(Name, counter)).
+-define(update_counter(Name), telemetry:update(Name, counter, 1)).
 -define(update_counter(Name, Value), telemetry:update(Name, counter, Value)).
 -define(update_histogram(Name, Value), telemetry:update(Name, histogram, Value)).
